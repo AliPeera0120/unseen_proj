2f5cdcaf6c1705bcb13241e717239814003ef42f
Alijawad Peera <apeera@engineering.upenn.edu>
2026-08-31 22:13:06 -0400

    no APi

--- src/components/home/MapPreview.jsx
+++ src/components/home/MapPreview.jsx
@@ -45,7 +45,7 @@ export default function MapPreview() {
           >
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+              url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             {locations.map((loc) => (
               <Marker key={loc.id} position={[loc.latitude, loc.longitude]}>
--- src/pages/DonationMap.jsx
+++ src/pages/DonationMap.jsx
@@ -122,7 +122,7 @@ export default function DonationMap() {
               >
                 <TileLayer
                   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                  url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+                  url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
                 {userLocation && !selected && (
                   <FlyToLocation lat={userLocation.lat} lng={userLocation.lng} />
